7a8f9ecb2d629984d5841657e6141b6043ad134f
whr0530 <bill.roske@daugherty.com>
2023-01-18 14:01:02 -0600

    Adding WebserviceTest to the Cucumber project Webservice tests may now be written in Cucumber.

---
+++ maqs-cucumber/src/test/java/com/cognizantsoftvision/maqs/cucumber/unittestpagemodel/DummyBaseWebServiceStep.java
@@ -0,0 +1,9 @@
+package com.cognizantsoftvision.maqs.cucumber.unittestpagemodel;
+
+import com.cognizantsoftvision.maqs.cucumber.steps.BaseWebServiceStep;
+
+/**
+ * A Dummy DummyBaseWebServiceStep class for testing
+ */
+public class DummyBaseWebServiceStep extends BaseWebServiceStep {
+}
