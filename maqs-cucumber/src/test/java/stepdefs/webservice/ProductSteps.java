c4b262ee08f62f8ab57311e324121fe893acfa30
Jason Edstrom <JasonE@magenic.com>
2023-01-20 11:52:30 -0600

    Testing if `cucumber-expressions` is breaking the tests

--- maqs-cucumber/src/test/java/stepdefs/webservice/ProductSteps.java
+++ maqs-cucumber/src/test/java/stepdefs/webservice/ProductSteps.java
@@ -14,16 +14,29 @@ import io.cucumber.java.en.Then;
 public class ProductSteps extends BaseWebServiceStep {
     private static final String baseUrl = WebServiceConfig.getWebServiceUri();
 
-    @Given("I Get Product Information for Product ID {int}")
-    public void i_get_product_information_for_product_id(Integer int1) throws IOException, InterruptedException {
+//    @Given("I Get Product Information for Product ID {int}")
+//    public void i_get_product_information_for_product_id(Integer int1) throws IOException, InterruptedException {
+//        Product product = this.getDriver().get(
+//            baseUrl + "/api/XML_JSON/GetProduct/"+int1, MediaType.APP_XML, true, Product.class);
+//        ScenarioContext.put("product", product);
+//
+//    }
+//    @Then("The Product Name is {string}")
+//    public void the_product_name_is(String pname) {
+//        Product p = (Product) ScenarioContext.get("product");
+//        Assert.assertEquals(p.getName(), pname, "Expected Yo-yo to be returned");
+//    }
+
+    @Given("^I Get Product Information for Product ID (\\d+)$")
+    public void iGetProductInformationForProductID(int arg0) throws IOException, InterruptedException {
         Product product = this.getDriver().get(
-            baseUrl + "/api/XML_JSON/GetProduct/2", MediaType.APP_XML, true, Product.class);
-        ScenarioContext.put("product", product);
-        
+                        baseUrl + "/api/XML_JSON/GetProduct/"+arg0, MediaType.APP_XML, true, Product.class);
+                    ScenarioContext.put("product", product);
     }
-    @Then("The Product Name is {string}")
-    public void the_product_name_is(String pname) {
-        Product p = (Product) ScenarioContext.get("product");
-        Assert.assertEquals(p.getName(), pname, "Expected Yo-yo to be returned");
+
+    @Then("^The Product Name is \"([^\"]*)\"$")
+    public void theProductNameIs(String pname) throws Throwable {
+                Product p = (Product) ScenarioContext.get("product");
+                Assert.assertEquals(p.getName(), pname, "Expected Yo-yo to be returned");
     }
 }
