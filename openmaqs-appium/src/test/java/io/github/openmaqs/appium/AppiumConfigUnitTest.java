64be66af171f5c2c5cf773342a90a3817e2c7de2
Jon Reding <redi0063@gmail.com>
2023-10-31 12:42:36 -0500

    some updates

--- openmaqs-appium/src/test/java/io/github/openmaqs/appium/AppiumConfigUnitTest.java
+++ openmaqs-appium/src/test/java/io/github/openmaqs/appium/AppiumConfigUnitTest.java
@@ -61,8 +61,8 @@ public class AppiumConfigUnitTest {
   @Test(groups = TestCategories.APPIUM)
   public void testGetMobileHubUrlString() {
     String mobileHubUrl = AppiumConfig.getMobileHubUrlString();
-    Assert.assertTrue(mobileHubUrl.contains("https://")
-        && mobileHubUrl.contains("ondemand.us-west-1.saucelabs.com:443/wd/hub"));
+    Assert.assertTrue(mobileHubUrl.contains("http://"));
+    Assert.assertTrue(mobileHubUrl.contains("ondemand.saucelabs.com:80/wd/hub"));
   }
 
   /**
